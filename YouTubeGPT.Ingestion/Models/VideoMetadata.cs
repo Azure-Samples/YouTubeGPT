a98fe12317f79a5002f584c26292a85e333847d3
Aaron Powell <me@aaron-powell.com>
2024-03-12 10:33:17 +1100

    fixing warnings

--- YouTubeGPT.Ingestion/Models/VideoMetadata.cs
+++ YouTubeGPT.Ingestion/Models/VideoMetadata.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using YoutubeExplode.Common;
+﻿using YoutubeExplode.Common;
 
 namespace YouTubeGPT.Ingestion.Models;
 internal class VideoMetadata
@@ -11,8 +6,8 @@ internal class VideoMetadata
     public string Title { get; set; } = null!;
     public string Description { get; set; } = null!;
     public TimeSpan? Duration { get; internal set; }
-    public Author Author { get; internal set; }
-    public string Url { get; internal set; }
+    public Author Author { get; internal set; } = null!;
+    public string Url { get; internal set; } = null!;
     public DateTimeOffset UploadDate { get; internal set; }
-    public IReadOnlyList<string> Keywords { get; internal set; }
+    public IReadOnlyList<string> Keywords { get; internal set; } = [];
 }
