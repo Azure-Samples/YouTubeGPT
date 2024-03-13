6ad9e5cc8f94695caa5d71fbd8415ccb37049016
Aaron Powell <me@aaron-powell.com>
2024-03-13 14:34:18 +1100

    Database migration complete

--- YouTubeGPT.DatabaseMigrator/Program.cs
+++ YouTubeGPT.DatabaseMigrator/Program.cs
@@ -2,7 +2,13 @@
 using Microsoft.Extensions.Hosting;
 using YouTubeGPT.Ingestion;
 
-var host = Host.CreateApplicationBuilder(args).Build();
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.AddServiceDefaults();
+
+builder.AddNpgsqlDbContext<MetadataDbContext>("metadata");
+
+var host = builder.Build();
 
 var db = host.Services.GetService(typeof(MetadataDbContext)) as MetadataDbContext
          ?? throw new ArgumentException();
