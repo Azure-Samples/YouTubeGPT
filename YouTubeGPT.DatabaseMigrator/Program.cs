3cfe2a05955eeec35de8789ae0f9481093f6c6d6
Aaron Powell <me@aaron-powell.com>
2024-05-20 11:07:17 +1000

    Improving resilliance in starting up the db migration command

--- YouTubeGPT.DatabaseMigrator/Program.cs
+++ YouTubeGPT.DatabaseMigrator/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using YouTubeGPT.Ingestion;
 using YouTubeGPT.Shared;
 
@@ -11,9 +13,38 @@ builder.AddNpgsqlDbContext<MetadataDbContext>(ServiceNames.MetadataDB);
 
 var host = builder.Build();
 
-var db = host.Services.GetService(typeof(MetadataDbContext)) as MetadataDbContext
+var db = host.Services.GetService<MetadataDbContext>()
          ?? throw new ArgumentException(nameof(MetadataDbContext));
 
+var connection = db.Database.GetDbConnection();
+var logger = host.Services.GetService<ILogger<Program>>()!;
+
+var isDbReady = false;
+var retryCount = 10;
+var tryCount = 0;
+
+while (!isDbReady && tryCount++ < retryCount)
+{
+    try
+    {
+        connection.Open();
+        isDbReady = true;
+        logger.LogInformation("Database connection opened successfully");
+    }
+    catch (Exception ex)
+    {
+        logger.LogWarning("Failed to open databse connection {Exception}", ex);
+        await Task.Delay(1000);
+    }
+}
+
+if (!isDbReady)
+{
+    logger.LogError("Failed to open database connection after {RetryCount} retries", retryCount);
+    return;
+}
+
 db.Database.Migrate();
+logger.LogInformation("Database migration completed successfully");
 
 host.Run();
