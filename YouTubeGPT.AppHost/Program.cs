97d9e0ebd46d40b67e2b91ae30104f0c9094008c
Aaron Powell <me@aaron-powell.com>
2024-07-21 07:18:32 +1000

    Supporting connection string rather than always deploying a resource

--- YouTubeGPT.AppHost/Program.cs
+++ YouTubeGPT.AppHost/Program.cs
@@ -9,11 +9,22 @@ var ai = builder.ExecutionContext.IsPublishMode ?
         .AddDeployment(new(builder.Configuration["Azure:AI:EmbeddingDeploymentName"] ?? "text-embedding-3-small", "text-embedding-3-small", "1")) :
     builder.AddConnectionString(ServiceNames.OpenAI);
 
-var databaseServer = builder.AddSqlServer("sql")
-    .AsAzureSqlDatabase();
 
-var vectorDB = databaseServer.AddDatabase(ServiceNames.VectorDB);
-var metadataDB = databaseServer.AddDatabase(ServiceNames.MetadataDB);
+IResourceBuilder<IResourceWithConnectionString> vectorDB;
+IResourceBuilder<IResourceWithConnectionString> metadataDB;
+if (builder.ExecutionContext.IsPublishMode)
+{
+
+    var databaseServer = builder.AddSqlServer("sql")
+        .AsAzureSqlDatabase();
+    vectorDB = databaseServer.AddDatabase(ServiceNames.VectorDB);
+    metadataDB = databaseServer.AddDatabase(ServiceNames.MetadataDB);
+}
+else
+{
+    vectorDB = builder.AddConnectionString(ServiceNames.VectorDB);
+    metadataDB = builder.AddConnectionString(ServiceNames.MetadataDB);
+}
 
 builder.AddProject<Projects.YouTubeGPT_Ingestion>("youtubegpt-ingestion")
     .WithReference(ai)
