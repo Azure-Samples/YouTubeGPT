b6ab7ffca8ce4fb96e17b58d4af674cea51422fb
Aaron Powell <me@aaron-powell.com>
2024-07-30 14:11:32 +1000

    Moving to nunit so playwright can be used

--- tests/YouTubeGPT.Tests/AppHostHealthCheckTests.cs
+++ tests/YouTubeGPT.Tests/AppHostHealthCheckTests.cs
@@ -1,18 +1,17 @@
-using Microsoft.Extensions.Logging;
-using Xunit.Abstractions;
 using YouTubeGPT.Shared;
+using YouTubeGPT.Tests.NUnitExtensions;
 
 namespace YouTubeGPT.Tests;
 
-public class AppHostHealthCheckTests(ITestOutputHelper testOutputHelper)
+public class AppHostHealthCheckTests
 {
-    [Fact]
+    [Test]
     public async Task ClientAppStartsAndRespondsWithOk()
     {
         var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.YouTubeGPT_AppHost>();
         appHost.Services
             .ConfigureHttpClientDefaults(clientBuilder => clientBuilder.AddStandardResilienceHandler())
-            .AddLogging(builder => builder.AddXUnit(testOutputHelper));
+            .AddLogging(builder => builder.AddNUnit());
 
         await using var app = await appHost.BuildAsync();
         var resourceNotificationService = app.Services.GetRequiredService<ResourceNotificationService>();
@@ -22,16 +21,16 @@ public class AppHostHealthCheckTests(ITestOutputHelper testOutputHelper)
         await resourceNotificationService.WaitForResourceAsync(ServiceNames.YouTubeGPTClient, KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
         var response = await httpClient.GetAsync("/");
 
-        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
     }
 
-    [Fact]
+    [Test]
     public async Task IngestionAppStartsAndRespondsWithOk()
     {
         var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.YouTubeGPT_AppHost>();
         appHost.Services
             .ConfigureHttpClientDefaults(clientBuilder => clientBuilder.AddStandardResilienceHandler())
-            .AddLogging(builder => builder.AddXUnit(testOutputHelper));
+            .AddLogging(builder => builder.AddNUnit());
 
         await using var app = await appHost.BuildAsync();
         var resourceNotificationService = app.Services.GetRequiredService<ResourceNotificationService>();
@@ -40,6 +39,6 @@ public class AppHostHealthCheckTests(ITestOutputHelper testOutputHelper)
         var httpClient = app.CreateHttpClient(ServiceNames.YouTubeGPTIngestion);
         await resourceNotificationService.WaitForResourceAsync(ServiceNames.YouTubeGPTIngestion, KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
         var response = await httpClient.GetAsync("/");
-        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
     }
 }
---
+++ tests/YouTubeGPT.Tests/NUnitExtensions/LogScope.cs
@@ -0,0 +1,35 @@
+﻿namespace YouTubeGPT.Tests.NUnitExtensions;
+
+internal class LogScope(object state)
+{
+    private static readonly AsyncLocal<LogScope?> _value = new();
+
+    public object State { get; } = state;
+
+    internal static LogScope? Current
+    {
+        get { return _value.Value; }
+        set { _value.Value = value; }
+    }
+
+    internal LogScope? Parent { get; private set; }
+
+    public override string? ToString() => State.ToString();
+
+    internal static IDisposable Push(object state)
+    {
+        var temp = Current;
+
+        Current = new LogScope(state)
+        {
+            Parent = temp,
+        };
+
+        return new DisposableScope();
+    }
+
+    private sealed class DisposableScope : IDisposable
+    {
+        public void Dispose() => Current = Current?.Parent;
+    }
+}
